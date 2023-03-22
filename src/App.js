ac4585aa49a1771670fd87fb1b884abcc25494bc
Matheus Alencar <matheusalencar6942@gmail.com>
2023-03-22 11:00:07 -0300

    invoices page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/dashboard";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/dashboard";
 // import Form from "./scenes/dashboard";
@@ -31,7 +31,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
-                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                <Route path="/invoices" element={<Invoices />} />
                 <Route path="/contacts" element={<Contacts />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/form" element={<Form />} /> */}
