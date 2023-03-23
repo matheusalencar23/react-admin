c7926bd3bc914ca8f970b1eef40d67fa842e39f3
Matheus Alencar <matheusalencar6942@gmail.com>
2023-03-23 15:36:01 -0300

    creating geo chart

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/dashboard";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -38,7 +38,7 @@ function App() {
                 <Route path="/line" element={<Line />} />
                 <Route path="/pie" element={<Pie />} />
                 <Route path="/faq" element={<FAQ />} />
-                {/* <Route path="/geography" element={<Geography />} /> */}
+                <Route path="/geography" element={<Geography />} />
                 <Route path="/calendar" element={<Calendar />} />
               </Routes>
             </main>
